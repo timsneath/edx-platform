4c9502daa383e49b46f8abec5456c271e5e24ccb
stephensanchez <steve@edx.org>
2014-12-11 14:35:49 +0000

    Re-ordering redirect urls to be evaluated last.

--- common/djangoapps/enrollment/urls.py
+++ common/djangoapps/enrollment/urls.py
@@ -18,20 +18,23 @@ USER_PATTERN = '(?P<user>[\w.+-]+)'
 urlpatterns = patterns(
     'enrollment.views',
     url(r'^user/{user}$'.format(user=USER_PATTERN), EnrollmentListView.as_view(), name='courseenrollments'),
-    url(r'^user', EnrollmentListRedirectView.as_view(), name='courseenrollmentsredirect'),
     url(
         r'^user/{user}/course/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN, user=USER_PATTERN),
         EnrollmentView.as_view(),
         name='courseenrollment'
     ),
-    url(
-        r'^course/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
-        EnrollmentRedirectView.as_view(),
-        name='courseenrollmentredirect'
-    ),
+
     url(
         r'^course/{course_key}/details$'.format(course_key=settings.COURSE_ID_PATTERN),
         EnrollmentCourseDetailView.as_view(),
         name='courseenrollmentdetails'
+    ),
+
+    # Keep all redirect views at the bottom of the list, otherwise they will be evaluated too soon.
+    url(r'^user', EnrollmentListRedirectView.as_view(), name='courseenrollmentsredirect'),
+    url(
+        r'^course/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
+        EnrollmentRedirectView.as_view(),
+        name='courseenrollmentredirect'
     ),
 )
