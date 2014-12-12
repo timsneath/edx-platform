48cb5668a5cdb1f34bf2bcbe6a7c71eccb6047f5
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2014-12-12 19:26:08 +0500

    Fixed unicode problem if transcript file is not UTF-8 encoded. TNL-527

--- common/lib/xmodule/xmodule/video_module/video_handlers.py
+++ common/lib/xmodule/xmodule/video_module/video_handlers.py
@@ -300,7 +300,14 @@ class VideoStudioViewHandlers(object):
 
             if request.method == 'POST':
                 subtitles = request.POST['file']
-                save_to_store(subtitles.file.read(), unicode(subtitles.filename), 'application/x-subrip', self.location)
+                try:
+                    file_data = subtitles.file.read()
+                    unicode(file_data, "utf-8", "strict")
+                except UnicodeDecodeError:
+                    msg = "Invalid encoding type for file: {}".format(subtitles.filename)
+                    log.info(msg)
+                    return Response(msg, status=400)
+                save_to_store(file_data, unicode(subtitles.filename), 'application/x-subrip', self.location)
                 generate_sjson_for_all_speeds(self, unicode(subtitles.filename), {}, language)
                 response = {'filename': unicode(subtitles.filename), 'status': 'Success'}
                 return Response(json.dumps(response), status=201)
